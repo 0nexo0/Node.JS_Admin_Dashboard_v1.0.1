def1a049c8b7252094d3e93e3d74ed472aa2e3b5
Gimhan Hashintha <133946828+0nexo0@users.noreply.github.com>
2024-06-27 11:44:33 +0530

    2024.06.27 11.44 AM

---
+++ controllers/insertMembersApiController.js
@@ -0,0 +1,44 @@
+import dbConnect from "../database/database.js";
+import { upload } from "../libs/multerConfig.js";
+
+const connection = dbConnect;
+
+const insertMembers = async (req, res) => {
+  const uploadSingle = upload.single("image");
+
+  uploadSingle(req, res, async (error) => {
+    if (error) {
+      res.status(400).json({ error: error.message });
+    }
+    const {
+      name,
+      position,
+      facebooklink,
+      twitterlink,
+      linkedinlink,
+      whatsappnumber,
+    } = req.body;
+    const imgurl = req.file ? req.file.path : null;
+
+    try {
+      const db = req.app.locals.db;
+      const [response] = await db.execute(
+        `CALL c_insertMembers(?,?,?,?,?,?,?)`,
+        [
+          name,
+          position,
+          facebooklink,
+          twitterlink,
+          linkedinlink,
+          whatsappnumber,
+          imgurl,
+        ]
+      );
+      res.send("Upload Success");
+    } catch (error) {
+      res.status(400).json({ error: error.message });
+    }
+  });
+};
+
+export { insertMembers };
--- routers/routes.js
+++ routers/routes.js
@@ -5,12 +5,17 @@ import {
   fetchComments,
   fetchMembers,
 } from "../controllers/fetchDataApiController.js";
+import { insertMembers } from "../controllers/insertMembersApiController.js";
 
 const route = express.Router();
 
+//Dashboard Data Fetch Api Routes
 route.get("/members", fetchMembers);
 route.get("/bookings", fetchBookings);
 route.get("/comments", fetchComments);
 route.get("/blogs", fetchBlogs);
+
+//Insert Members Api Route
+route.post("/insertmembers", insertMembers);
 
 export default route;
